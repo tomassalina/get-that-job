5b8c2417f87ca19dbd9a0bac0eab74d5c9ac7783
Tomás Salina <salinatomass53@gmail.com>
2022-09-20 22:24:49 -0300

    React Router v6

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,7 +1,18 @@
 import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <h1>The place where you get that job</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<h1>The place where you get that job</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
