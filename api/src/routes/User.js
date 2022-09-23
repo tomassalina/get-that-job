c3a71faa26ca854111e6c4d3e2ad41d8813476c2
Marquisl2 <marcoslaurens25@gmail.com>
2022-09-23 16:49:23 -0300

    add routes

--- api/src/routes/User.js
+++ api/src/routes/User.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { createUser } = require('../controllers/user');
+const { createUser, updateUser } = require('../controllers/user');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,14 +9,25 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.post("/create", (req,res)=>{
+router.post("/create", async(req,res)=>{
     const {email, fullname} = req.body
 
     try {
-        const result = createUser(email,fullname)
+        const result = await createUser(email,fullname)
         res.send(result)
     } catch (error) {
         res.status(404).send("Algo salio mal!")
+    }
+})
+
+router.put("/update",async(req,res)=>{
+    const {email} = req.body
+
+    try {
+        const result = await updateUser(email)
+        res.send(result)
+    } catch (error) {
+        res.status(404).send(error)
     }
 })
 
