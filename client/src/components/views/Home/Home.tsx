162d82bb282e2d3ebc26b35cf5c4211c60f0a808
Tomás Salina <salinatomass53@gmail.com>
2022-10-10 23:49:52 -0300

    Protected layout and useUser

--- client/src/components/views/Home/Home.tsx
+++ client/src/components/views/Home/Home.tsx
@@ -1,11 +1,24 @@
-import { Navbar } from "../../Navbar"
-import { FindThatJob } from "../FindThatJob"
-import "./Home.styles.scss"
-export const Home = () => {
-  return (
-    <div className="home__container">
-    
-    
-    </div>
-  )
+import './Home.styles.scss'
+import { User } from '../../../hooks/useUser'
+
+interface HomeProps {
+  user: User
+}
+
+export const Home = ({ user }: HomeProps) => {
+  if (user.role === 'professional')
+    return (
+      <div className="home__container">
+        <h1>Professional</h1>
+      </div>
+    )
+
+  if (user.role === 'recruiter')
+    return (
+      <div className="home__container">
+        <h1>Recruiter</h1>
+      </div>
+    )
+
+  return <p>Loading...</p>
 }
